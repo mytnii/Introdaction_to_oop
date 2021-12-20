b2c0c1f2a011e3af859f74266ab874d5fdb1c0b3
mytnii <mytniigemoroi@gmail.com>
2021-12-20 22:11:06 +0300

    MethodsOutClass

--- class_string/main.cpp
+++ class_string/main.cpp
@@ -6,119 +6,151 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+#define delimiter cout << "\n-----------------------------------------------------------\n";
+
 class String;
 String operator+(const String& left, const String& right);
 
-#define delimiter cout << "\n-----------------------------------------------------------\n";
 
 class String
 {
 	int size;  //Размер строки
 	char* str; //Адрес строки в динамической памяти
 public:
-	const char* get_str()const
-	{
-		return str;
-	}
-	 char* get_str()
-	{
-		return str;
-	}
+	const char* get_str()const;
+	char* get_str();
 
 
-	 int get_size()const
-	{
-		return size;
-	}
+	int get_size()const;
 
 
 
-	 void set_size(int size)
-	 {
-		 this->size = size;
-	 }
+	void set_size(int size);
 
-	 explicit String(int size = 80) :size(size), str(new char[size] {})
-	{
-		//this->size = size;
-		//this->str = new char[size] {};  //Память выделяемую для строки обязательно нужно занулить
-		cout << "SizeConstructor:\t" << this << endl;
-	}
+	explicit String(int size = 80);
 
-	String(const char str[]):String(strlen(str) + 1)
-	{
-		//this->size = strlen(str) + 1;
-		//this->str = new char[size] {};
-		for (int i = 0; str[i]; i++)
-		{
-			this->str[i] = str[i];
-		}
-		cout << "Constructor:\t" << this << endl;
-	}
+	String(const char str[]);
 
-	String(const String& other):String(other.str)
-	{
-		/*this->size = other.size;
-		this->str = new char[size] {};
-		for (int i = 0; i < size; i++)
-		{
-			this->str[i] = other.str[i];
-		}*/
-		cout << "CopyConstructor:\t" << this << endl;
-	}
+	String(const String& other);
 
-	~String()
-	{
-		delete[] str;
-		cout << "Destructor:\t" << this << endl;
-	}
+	~String();
 
 	//-------------------Operators------------
-	String& operator=(const String& other)
-	{
-		//if (this == &other)
-		//{
-		//	return *this;
-		//}
-		delete[] this->str;
-		this->size = other.size;
-		this->str = new char[size] {};
-		for (int i = 0; i < size; i++)
-		{
-			this->str[i] = other.str[i];
-		}
-		cout << "CopyAssignment:\t" << this << endl;
-		return *this;
-	}
+	String& operator=(const String& other);
 
-	String& operator+=(const String& other)
-	{
-		return *this = *this + other;
-	}
+	String& operator+=(const String& other);
 
 
-	char& operator[](int i)
-	{
-		return str[i];
-	}
-	const char& operator[](int i)const
-	{
-		return str[i];
-	}
+	char& operator[](int i);
+	const char& operator[](int i)const;
 
 
 	//-------------------Methods---------------
 
-	void print()
-	{
-		cout << "Size:\t" << size << endl;
-		cout << "Str:\t" << str << endl;
-
-	}
+	void print()const;
 
 
 
 };
+
+const char* String::get_str()const
+{
+	return str;
+}
+char* String::get_str()
+{
+	return str;
+}
+
+int String::get_size()const
+{
+	return size;
+}
+
+
+
+
+ String::String(int size) :size(size), str(new char[size] {})
+{
+	//this->size = size;
+	//this->str = new char[size] {};  //Память выделяемую для строки обязательно нужно занулить
+	cout << "SizeConstructor:\t" << this << endl;
+}
+
+String::String(const char str[]) :String(strlen(str) + 1)
+{
+	//this->size = strlen(str) + 1;
+	//this->str = new char[size] {};
+	for (int i = 0; str[i]; i++)
+	{
+		this->str[i] = str[i];
+	}
+	cout << "Constructor:\t" << this << endl;
+}
+
+String::String(const String& other) :String(other.str)
+{
+	/*this->size = other.size;
+	this->str = new char[size] {};
+	for (int i = 0; i < size; i++)
+	{
+		this->str[i] = other.str[i];
+	}*/
+	cout << "CopyConstructor:\t" << this << endl;
+}
+
+String::~String()
+{
+	delete[] str;
+	cout << "Destructor:\t" << this << endl;
+}
+
+//-------------------Operators------------
+String& String::operator=(const String& other)
+{
+	//if (this == &other)
+	//{
+	//	return *this;
+	//}
+	delete[] this->str;
+	this->size = other.size;
+	this->str = new char[size] {};
+	for (int i = 0; i < size; i++)
+	{
+		this->str[i] = other.str[i];
+	}
+	cout << "CopyAssignment:\t" << this << endl;
+	return *this;
+}
+
+String& String::operator+=(const String& other)
+{
+	return *this = *this + other;
+}
+
+
+char& String::operator[](int i)
+{
+	return str[i];
+}
+const char& String::operator[](int i)const
+{
+	return str[i];
+}
+
+
+//-------------------Methods---------------
+
+void String::print()const
+{
+	cout << "Size:\t" << size << endl;
+	cout << "Str:\t" << str << endl;
+
+}
+
+
+
+
 
 
 String operator+(const String& left, const String& right)
